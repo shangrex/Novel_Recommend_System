5c8eec068d3591d39eeb78331b2f8bd89bdb12fa
shangrex <rexhuang881019@gmail.com>
2021-03-31 19:49:19 +0800

    Fix size of poet's data seperation

--- preprocess/split_data.py
+++ preprocess/split_data.py
@@ -4,7 +4,7 @@ data = pd.read_csv('data/poet.csv', index_col=False)
 print("before data")
 print(data.info())
 print(data.describe())
-split_len = 150000
+split_len = 20000
 for i in range(0, len(data), split_len):
     tmp = data[i:i+split_len]
     print(i)
