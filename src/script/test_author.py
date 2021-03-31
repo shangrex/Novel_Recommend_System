1917ca9bc1bfe66c6e3e0a9ee43d159d3554719b
shangrex <rexhuang881019@gmail.com>
2021-03-31 20:54:58 +0800

    Add topk prediction

--- src/script/test_author.py
+++ src/script/test_author.py
@@ -25,37 +25,45 @@ limit_number = args.limit_number
 
 tokenizer =  AutoTokenizer.from_pretrained('bert-base-chinese')
 
-model = BertForMaskedLM.from_pretrained(model_path)
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
         
 poem_dset = chpoemdset("test", tokenizer, limit_number)
 
 
 BATCH_SIZE = 1
 trainloader = DataLoader(poem_dset, batch_size=BATCH_SIZE, 
-                         collate_fn=poem_dset.create_mini_batch)
+                         collate_fn=poem_dset.create_mini_batch
+                         ,shuffle=True)
 
 model.eval()
+topk = 3
 
-dataiter = iter(trainloader)
+for m in range(20):
+    print("test index: {}".format(m))
+    dataiter = iter(trainloader).next()
 
-token_tensor , mask_tensor, lbl_tensor = dataiter[:3]
+    token_tensor , mask_tensor, lbl_tensor = dataiter[:3]
 
-outputs = model(input_ids = token_tensor,
-                attention_mask = mask_tensor)
+    outputs = model(input_ids = token_tensor,
+                    attention_mask = mask_tensor)
 
 
-logits = outputs[0]
-_, pred = torch.max(logits.data, 1)
-for i, j, k in zip(token_tensor, lbl_tensor, pred):
-    print(i)
-    print(poem_dset.dtknz(i))
-    print(k)
-    print("predict", torch.argmax(k))
-    # k = torch.zeros(300)
-    # k[torch.argmax(k)] = 1
-    # print(poem_dset.dauthor(k))
-    print(j)
-    print("ans", torch.argmax(j))
-    print(poem_dset.dauthor(j))
-    print(j.shape)   
-            
+    logits = outputs[0]
+    # print("logits", type(logits))
+    # print(logits.data)
+    # _, pred = torch.max(logits.data, 1)
+    predict_values, predict_indexes = torch.topk(logits.data, topk)
+    predict_indexes = predict_indexes[0]
+    for i, j in zip(token_tensor, lbl_tensor):
+        # print(i)
+        print(poem_dset.dtknz(i))
+        for k in range(topk):
+            index = predict_indexes[k].item()
+            print("predict index", index)
+            predict_author = torch.zeros(300)
+            predict_author[index] = 1
+            print("predict author", poem_dset.dauthor(predict_author))
+        # print(j)
+        print("ans", torch.argmax(j))
+        print(poem_dset.dauthor(j))
+                
