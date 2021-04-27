00f113b4fc21575cd5a6f93d38a9fa4e1de8e832
shangrex <rexhuang881019@gmail.com>
2021-04-27 17:44:27 +0800

    Fix search eigine of poet

--- src/script/run_search_poet.py
+++ src/script/run_search_poet.py
@@ -1,6 +1,7 @@
 '''
 Implement a search engine.
 '''
+from tqdm import tqdm
 import pandas as pd 
 import argparse
 
@@ -9,34 +10,46 @@ parser = argparse.ArgumentParser(description='fill arguement')
 
 parser.add_argument('--find', type=str, required=True,
                     help='the word for searching')
+parser.add_argument('--min', required=False, type=int, default=1,
+                    help="the threshold of match count")
 
 args = parser.parse_args()
 
 target = args.find.split(' ')
 print(target)
-
+min_count = args.min
 
 poet = pd.read_csv('data/poet.csv')
 
-result = {'author':[], 'paragraphs':[]}
 
-print("=="*7+"finding"+"=="*7)
-for i in range(len(poet)):
-    check_count = 0
+rst_atr = []
+print("=="*7+"searching author"+"=="*7)
+for i in tqdm(range(len(poet))):
     for t in target:
         if poet['author'].iloc[i] == t:
-            check_count += 1
-        if t in poet['paragraphs'].iloc[i]:
-            check_count += 1    
-    if check_count >= len(target):
-        result['author'].append({poet['author'].iloc[i]:poet['paragraphs'].iloc[i]})
-        result['paragraphs'].append({poet['author'].iloc[i]:poet['paragraphs'].iloc[i]})
+            rst_atr.append({t:poet['paragraphs'].iloc[i]})
 
-print("=="*7+"result"+"=="*7)
-for i, j in result.items():
-    print("=="*7,"search type", i, "=="*7)
-    print("searching result", len(j))
-    for k in j:
-        print(k)
-    
+print("=="*7+"searching paragraphs"+"==")
+rst_cnt = []
+for i in tqdm(range(len(poet))):
+    check_count = 0
+    for t in target:
+        if t in poet['paragraphs'].iloc[i]:
+            check_count += 1
+    if check_count > 0:
+        rst_cnt.append({'match_count':check_count, poet['author'].iloc[i]:poet['paragraphs'].iloc[i]})
+#sort 
+sorted(rst_cnt, key=lambda x: x['match_count'])
+
+print("=="*7+"result author"+"=="*7)
+for i in rst_atr:
+    print(i)
+
+print("=="*7+"result content"+"=="*7)
+for i in rst_cnt:
+    if i['match_count'] < min_count:
+        break
+    for j, k in i.items():
+        print(j, k)
+
 
